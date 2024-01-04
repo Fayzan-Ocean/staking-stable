dd88cdb664e7bb0946133d1c4042145f46fa3f7e
Faizan Asad <p176056@nu.edu.pk>
2024-01-04 18:57:07 +0500

    das

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,44 @@
-
+'use client'
+import Link from 'next/link';
+import { useAccount } from 'wagmi';
 import { Button } from './ui/button';
 
 
 
-
 const Navbar = () => {
-
+  
+  const { address, isConnecting, isDisconnected } = useAccount()
+  
+  const owner = "0x7496E853013eE234301C80848C2e4945f2a52980"
 
 
     return ( <>
-    <div className=" flex flex-wrap py-4 justify-end px-4 "> 
+    <div className=" flex flex-wrap py-4 justify-between px-4 "> 
+
+{!isDisconnected  ? 
+
+<> 
+{address == owner ? <><div className='container flex justify-start'>
+      <Link href='/transactions'><Button className='' variant={"outline"}> Transactions</Button>
+      </Link>
+</div></> : <><div></div></>}
+
+    
+  </>
+    
+    : <><div></div></>}
+   
+
+
     
     <div className=" flex justify-end h-14 py-4 px-4">
         
 
 
       <w3m-button />
-    </div></div>
+    </div>
+    
+    </div>
    
 
 
--- hooks/useAllTransactions.ts
+++ hooks/useAllTransactions.ts
@@ -9,6 +9,31 @@ const useAllTransactions = () => {
     const [loading, setLoading] = useState(true);
     // State to handle error state
     const [error, setError] = useState<string | null>(null);
+
+    const refreshTransactions = async () =>{
+      try {
+        // Fetch data from the API
+    
+        const response = await fetch(`/api/transactions`); // Update the API endpoint accordingly
+        const data = await response.json();
+
+        //console.log(data)
+
+        // Check if the request was successful
+        if (response.ok) {
+          setTransactions(data.sortedTxs);
+        } else {
+          // Handle API error
+          setError(data.message || 'Failed to fetch transactions');
+        }
+      } catch (error) {
+        // Handle general fetch error
+        setError('Failed to fetch transactions');
+      } finally {
+        // Set loading to false once the request is complete
+        setLoading(false);
+      }
+    }
   
     useEffect(() => {
       const fetchData = async () => {
@@ -43,7 +68,7 @@ const useAllTransactions = () => {
       fetchData();
     }, []); // Empty dependency array ensures that this effect runs once when the component mounts
   
-    return { transactions, loading, error };
+    return { transactions, loading, error, refreshTransactions };
   };
   
   export default useAllTransactions;
