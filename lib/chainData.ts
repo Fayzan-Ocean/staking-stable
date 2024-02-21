2d67f9fbab8ec0dc3c93b5f0cf17c3677367c39b
Fayzan_Ocean <faizan@ocean.money>
2024-02-21 13:36:31 +0500

    new contract

--- lib/chainData.ts
+++ lib/chainData.ts
@@ -10,14 +10,14 @@ export const networkData = [
         chainId: 137,
         usdcAddress:'0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
         usdtAddress:'0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
-        dappContract:'0x3Bfe51755849eadBF8bC197263f309Eb18007F18',
+        dappContract:'0x406D8a43c3FD5d31CA6416650DBBc69C04D7881B',
         explorer:'http://polygonscan.com/'
     },
     {
         chainId: 80001,
         usdcAddress:'0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747',
         usdtAddress:'0xe4c2ba0B2ad6d3076d58e1268ef5B4B49CD2E6F4',
-        dappContract:'0xf361ca23444fD658ECc724dABaedC9D822398e36',
+        dappContract:'0xDb8dEA9e93aDAa7D1Bf079a13C29C2EF2aD9a645',
         explorer:'http://mumbai.polygonscan.com/'
     }
 
